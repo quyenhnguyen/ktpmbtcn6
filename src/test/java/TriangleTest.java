ba44eb8d5169c269ce944054cb02eb79122724b1
Quyen Nguyen <hoangquyen7901@gmail.com>
2020-12-26 13:18:53 +0700

    Add 5 testcase

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -3,18 +3,24 @@ import org.junit.Test;
 
 
 public class TriangleTest {
-    public double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        long factor = (long) Math.pow(10, places);
-        value = value * factor;
-        long tmp = Math.round(value);
-        return (double) tmp / factor;
-    }
     @Test
     public void Testcase1() {
         Diem A=new Diem(4.3,5);
         Diem B=new Diem(1,-2.4);
         double d=A.tinhKhoangCach(B);
-        Assert.assertEquals(d,8.10246,0.00001); }
+        Assert.assertEquals(d,8.10246,0.00001);
+    }
+    @Test
+    public void Testcase2() {
+        Diem A=new Diem(0,0);
+        Diem B=new Diem(0,0);
+        double d=A.tinhKhoangCach(B);
+        Assert.assertEquals(d,0,0.00001);
+    }
+    @Test
+    public void Testcase3() {
+        TamGiac t =new TamGiac(new Diem(0,0), new Diem(5,0), new Diem(0,5));
+        Assert.assertEquals("Tam giac vuong can", t.LoaiTamGiac());
+    }
+
 }
